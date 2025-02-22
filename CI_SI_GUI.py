87a00c55bcbe296c4b2b63ffc3c6a9caf2d7c838
Prakhar Singh <101881757+Hawk577@users.noreply.github.com>
2025-02-23 01:14:59 +0530

    Update CI_SI_GUI.py
    added basic functionality

--- CI_SI_GUI.py
+++ CI_SI_GUI.py
@@ -1,14 +1,64 @@
-import tkinter as tk 
+import tkinter as tk
 window = tk.Tk()
-window.title("Simple and Compound Interest Calculator")
-window.geometry("600x400")
-welcome = tk.Label(window, text ='''
-                   Welcome to Simple Interest and Compound Interest calculator
-                                made by Hawk577
-                   ''')
-welcome.grid(row = 0, column=0, columnspan=3, pady=10)
-P = tk.Label(window, text = "Please enter Principal amount ")
-P.grid(row=1, column=0)
-p = tk.Entry(window)
-p.grid(row = 1, column=1, columnspan =2, padx= 10, pady = 10)
+welcome= tk.Label(window,text="Welcome to SI and CI calculator")
+creator = tk.Label(window, text = "By Hawk577")
+pStatement= tk.Label(window, text="Please enter principal amount: ")
+rStatement = tk.Label(window, text="Please enter rate of interest: ")
+tStatement = tk.Label(window, text="Please enter time period (in years): ")
+result = tk.Label(window, text="")
+Error= tk.Label(window, text="")
+pEntry =  tk.Entry(window)
+rEntry =  tk.Entry(window)
+tEntry =  tk.Entry(window)
+def si_calculate(pEntry, rEntry, tEntry, Error, result):
+    try:
+        principal= float(pEntry.get())
+        rate= float(pEntry.get())
+        time= float(pEntry.get())
+        res = (principal*rate*time)/100
+        result.config(text=f"Simple Interest is {res}")
+    except:
+        Error.config(text="Please enter numbers only in the input fields !")
+        pEntry.delete(0, tk.END)
+        tEntry.delete(0, tk.END)
+        rEntry.delete(0, tk.END)
+        principal = 0
+        time = 0
+        rate = 0
+def ci_calculate(pEntry, rEntry, tEntry, Error, result):
+    try:
+        #CI = P*(1+R/100)**T-P
+        principal= float(pEntry.get())
+        rate= float(pEntry.get())
+        time= float(pEntry.get())
+        res = (principal*((1+(rate/100))**time)-principal)
+        result.config(text=f"Simple Interest is {res}")
+    except:
+        Error.config(text="Please enter numbers only in the input fields !")
+        pEntry.delete(0, tk.END)
+        tEntry.delete(0, tk.END)
+        rEntry.delete(0, tk.END)
+        principal = 0
+        time = 0
+        rate = 0
+
+window.title("basic CI and SI calculator")
+principalClear = tk.Button(window, text="clear", command =lambda: pEntry.delete(0, tk.END))
+rateClear = tk.Button(window, text="clear", command =lambda: rEntry.delete(0, tk.END))
+timeClear = tk.Button(window, text="clear", command =lambda: tEntry.delete(0, tk.END))
+welcome.grid()
+creator.grid()
+pStatement.grid()
+pEntry.grid()
+principalClear.grid()
+rStatement.grid()
+rEntry.grid()
+rateClear.grid()
+tStatement.grid()
+tEntry.grid()
+timeClear.grid()
+
+def calculate():
+    pass
 window.mainloop()
+
