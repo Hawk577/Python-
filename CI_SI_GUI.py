479d7798d90a7965a0ea62dc35be29c17c5d84ad
Prakhar Singh <101881757+Hawk577@users.noreply.github.com>
2025-02-26 12:12:22 +0000

    Update CI_SI_GUI.py

--- CI_SI_GUI.py
+++ CI_SI_GUI.py
@@ -10,6 +10,17 @@ Error= tk.Label(window, text="")
 pEntry =  tk.Entry(window)
 rEntry =  tk.Entry(window)
 tEntry =  tk.Entry(window)
+Calctype = str
+si_radio = tk.Radiobutton(window, text="Simple Interest", variable=Calctype, value="SI")
+ci_radio = tk.Radiobutton(window, text="Compound Interst", variable=Calctype, value= "CI")
+si_radio.grid()
+ci_radio.grid()
+def preCalcFunc():
+    try:
+        pass
+    except:
+        pass
+calculateBtn = tk.Button(window, text="Calculate", command=preCalcFunc())
 def si_calculate(pEntry, rEntry, tEntry, Error, result):
     try:
         principal= float(pEntry.get())
@@ -46,19 +57,18 @@ window.title("basic CI and SI calculator")
 principalClear = tk.Button(window, text="clear", command =lambda: pEntry.delete(0, tk.END))
 rateClear = tk.Button(window, text="clear", command =lambda: rEntry.delete(0, tk.END))
 timeClear = tk.Button(window, text="clear", command =lambda: tEntry.delete(0, tk.END))
-welcome.grid()
-creator.grid()
-pStatement.grid()
-pEntry.grid()
-principalClear.grid()
-rStatement.grid()
-rEntry.grid()
-rateClear.grid()
-tStatement.grid()
-tEntry.grid()
-timeClear.grid()
+welcome.grid(row= 0, column = 1,columnspan=2,padx=10,pady=10)
+creator.grid(row = 1, column = 1,columnspan=2,padx=10,pady=10)
+pStatement.grid(row  =3, column = 0,padx=10,pady=10)
+pEntry.grid(row=3, column= 1,padx=10,pady=10)
+principalClear.grid(row= 3, column= 2,padx=10,pady=10)
+rStatement.grid(row =4, column =0,padx=10,pady=10 )
+rEntry.grid(row=4, column= 1,padx=10,pady=10)
+rateClear.grid(row= 4, column=2,padx=10,pady=10)
+tStatement.grid(row=5,column=0,padx=10,pady=10)
+tEntry.grid(row=5, column=1,padx=10,pady=10)
+timeClear.grid(row=5, column=2,padx=10,pady=10)
 
 def calculate():
     pass
 window.mainloop()
-
