49d88137c1f7aeff1772af19aacd4be4be8243c6
Prakhar Singh <101881757+Hawk577@users.noreply.github.com>
2025-03-01 22:53:33 +0530

    Update CI_SI_GUI.py

--- CI_SI_GUI.py
+++ CI_SI_GUI.py
@@ -10,17 +10,19 @@ Error= tk.Label(window, text="")
 pEntry =  tk.Entry(window)
 rEntry =  tk.Entry(window)
 tEntry =  tk.Entry(window)
-Calctype = str
+Calctype = tk.StringVar(value="SI")
 si_radio = tk.Radiobutton(window, text="Simple Interest", variable=Calctype, value="SI")
 ci_radio = tk.Radiobutton(window, text="Compound Interst", variable=Calctype, value= "CI")
-si_radio.grid()
-ci_radio.grid()
-def preCalcFunc():
-    try:
-        pass
-    except:
-        pass
-calculateBtn = tk.Button(window, text="Calculate", command=preCalcFunc())
+si_radio.grid(row = 2, column=0,columnspan=1, padx=10, pady=10)
+ci_radio.grid(row = 2, column=1, columnspan=2, padx=10, pady=10)
+def preCalcFunc(i):
+        if i=="SI":
+            si_calculate(pEntry, rEntry, tEntry, Error, result)
+        elif i=="CI":
+            ci_calculate(pEntry, rEntry, tEntry, Error, result)
+        else:
+            Error.config(text="Please select a type of interest !")
+calculateBtn = tk.Button(window, text="Calculate", command=lambda: preCalcFunc(Calctype.get()))
 def si_calculate(pEntry, rEntry, tEntry, Error, result):
     try:
         principal= float(pEntry.get())
@@ -53,12 +55,14 @@ def ci_calculate(pEntry, rEntry, tEntry, Error, result):
         time = 0
         rate = 0
 
+def Reset():
+    pass
 window.title("basic CI and SI calculator")
 principalClear = tk.Button(window, text="clear", command =lambda: pEntry.delete(0, tk.END))
 rateClear = tk.Button(window, text="clear", command =lambda: rEntry.delete(0, tk.END))
 timeClear = tk.Button(window, text="clear", command =lambda: tEntry.delete(0, tk.END))
-welcome.grid(row= 0, column = 1,columnspan=2,padx=10,pady=10)
-creator.grid(row = 1, column = 1,columnspan=2,padx=10,pady=10)
+welcome.grid(row= 0, column = 0,columnspan=2,padx=10,pady=10)
+creator.grid(row = 1, column = 0,columnspan=2,padx=10,pady=10)
 pStatement.grid(row  =3, column = 0,padx=10,pady=10)
 pEntry.grid(row=3, column= 1,padx=10,pady=10)
 principalClear.grid(row= 3, column= 2,padx=10,pady=10)
@@ -68,7 +72,9 @@ rateClear.grid(row= 4, column=2,padx=10,pady=10)
 tStatement.grid(row=5,column=0,padx=10,pady=10)
 tEntry.grid(row=5, column=1,padx=10,pady=10)
 timeClear.grid(row=5, column=2,padx=10,pady=10)
-
+calculateBtn.grid(row=6, column=2, padx=10, pady=10)
+result.grid(row=6, column=0, padx=10, pady=10)
+Error.grid(row=6, column=0, padx=10, pady=10)
 def calculate():
     pass
 window.mainloop()
